c58afe68cef434728387f6beba4a78f5801451ae
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-03-19 00:28:54 +0300

    add car and rings

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Canvas } from "@react-three/fiber";
 import "./style.css";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Ground } from "./Ground";
-
+import { Chevrolette } from "./Chevrolet";
+import { Rings } from "./Rings";
 function CarShow() {
   return (
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
       <color args={[0, 0, 0]} attach="background" />
-
+      <Chevrolette />
+      <Rings />
       <spotLight
         color={[1, 0.25, 0.7]}
         intensity={1.5}
---
+++ src/Chevrolet.jsx
@@ -0,0 +1,24 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Mesh } from "three";
+export function Chevrolette() {
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/chevrolet_corvette_c7/scene.gltf"
+  );
+
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.traverse((object) => {
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+
+  return <primitive object={gltf.scene} />;
+}
